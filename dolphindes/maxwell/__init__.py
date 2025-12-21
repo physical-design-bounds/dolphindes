d70ad192d34c3980fe204c8435254e5d256ad83e
Alessio Amaolo <alessioamaolo@gmail.com>
2025-12-21 14:03:08 -0300

    add polar solver to init

--- dolphindes/maxwell/__init__.py
+++ dolphindes/maxwell/__init__.py
@@ -1,5 +1,11 @@
 """Public interface for TM_FDFD."""
 
 from .maxwell_fdfd import TM_FDFD
+from .maxwell_polar_fdfd import (
+    TM_Polar_FDFD,
+)
 
-__all__ = ["TM_FDFD"]
+__all__ = [
+    "TM_FDFD",
+    "TM_Polar_FDFD",
+]
